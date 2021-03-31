b370e597a7b63be2c562c00f8a0ae4cdfb4eab39
austinkret <austinkret@gmail.com>
2021-03-31 00:06:13 +0000

    eqArrays.js files

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arrOne, arrTwo) => {
+  if(arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if(arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
