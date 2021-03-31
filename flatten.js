926ecf448e621060b0b27cf59686769d3216ded4
austinkret <austinkret@gmail.com>
2021-03-31 19:01:21 +0000

    flatten file

--- flatten.js
+++ flatten.js
@@ -1,23 +1,23 @@
-// const eqArrays = (arrOne, arrTwo) => {
-//   if (arrOne.length === arrTwo.length) {
-//     for (let i = 0; i < arrOne.length; i++) {
-//       if (arrOne[i] !== arrTwo[i]) {
-//         return false;
-//       }
-//     }
-//   } else {
-//     return false;
-//   }
-//   return true;
-// };
+const eqArrays = (arrOne, arrTwo) => {
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
 
-// const assertArraysEqual = (arrOne, arrTwo) => {
-//   if (eqArrays(arrOne, arrTwo)) {
-//     console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
-//   }
-// };
+const assertArraysEqual = (arrOne, arrTwo) => {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
 
 const flatten = (arrOne) => {
   let flatArray = [];
@@ -32,4 +32,4 @@ const flatten = (arrOne) => {
     return flatArray;
   };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([1, 2, [3, 4], 5, [6], [2, 4, [6, 5,[5]]]]));
