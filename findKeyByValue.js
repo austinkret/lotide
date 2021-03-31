4108a3730953128b0a1484aa6020b5ab76760978
austinkret <austinkret@gmail.com>
2021-03-31 22:24:51 +0000

    findKeyByValue doc

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+//function findKeyByValue which takes in an object and a value.
+  const keys = Object.keys(object);  
+  for (const subject of keys) {
+    if (object[subject] === value) {
+      return subject
+      //scan the object and return the first key which contains the given value.
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
