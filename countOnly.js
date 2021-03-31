7626f41306c3f92d622b23811f5f75fdf560b7d9
austinkret <austinkret@gmail.com>
2021-03-31 20:30:43 +0000

    countOnly file

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Process an array and an object
+//return an object
+//object should contain counts of everything that the input object listed
+// only kjeys with a truthy value should be counted
+//all other strings should NOT be counted
+// if there is a key in the object that is truthy, but value is 0, do not return the value
+// report back how many instances of each string were found in the allItems array of strings
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+    for(const item of allItems) {
+      if(itemsToCount[item]) {
+        if(results[item]) {
+          results[item] += 1;
+        } else {
+          results[item] = 1;
+        }
+      }
+    }
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
