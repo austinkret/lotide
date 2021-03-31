55b9ccb4c2f39d6043876b1aeb99bef88ac8d0c8
austinkret <austinkret@gmail.com>
2021-03-31 22:24:26 +0000

    letterPositions doc

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = (arrOne, arrTwo) => {
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arrOne, arrTwo) => {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+/*
+For each letter, instead of returning just one number to represent its number of occurrences, 
+multiple numbers may be needed to represent all the places in the string that it shows up.
+*/
+
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+
+
+assertArraysEqual(letterPositions('lighthouse in the house'), [1]);
+
+assertArraysEqual(letterPositions('hello'), [1]);
