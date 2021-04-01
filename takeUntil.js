66f9529786f6c0cfcd2b9c3c7f00d4d9fc920119
austinkret <austinkret@gmail.com>
2021-04-01 22:58:49 +0000

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+const eqArrays = (arrOne, arrTwo) => {
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arrOne, arrTwo) => {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+//two parameters - array to work with, and the callback
+const takeUntil = (array, callback) => {
+  //return a "slice of the array with elements taken from the beginning"
+  const results = []; //new array to fill values
+  for (let element of array) {
+    if (callback(element) === false) {
+      results.push(element);
+    } if (callback(element) === true) {
+      break;
+    }
+  }
+  return results;
+};
+
+//callback should only return one item
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
