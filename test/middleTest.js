8a4b7d51d8f299ef165bbe1c277f2ac4554ae1ad
austinkret <austinkret@gmail.com>
2021-04-05 20:59:49 +0000

    updated with module exports

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle')
+const middle = require('../middle');
 
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
