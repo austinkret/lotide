d65f7744fc23fe6c4551eeb767ee461522b3c347
austinkret <austinkret@gmail.com>
2021-04-06 00:00:28 +0000

    updated mocha chai

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
 
 module.exports = {
   head,
@@ -23,15 +12,4 @@ module.exports = {
   assertArraysEqual,
   assertEqual,
   eqArrays,
-  assertObjectsEqual,
-  countLetters,
-  countOnly,
-  eqObjects,
-  findKey,
-  findKeyByValue,
-  flatten,
-  letterPositions,
-  map,
-  takeUntil,
-  without
 };
